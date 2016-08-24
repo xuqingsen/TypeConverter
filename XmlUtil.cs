92142f362b708191564129c5bbe7b36127e69a1b
xuqingsen <gulangduxiao@gmail.com>
2016-08-24 17:03:41 +0800

    xx
    xx

---
+++ XmlUtil.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace YunStore.Common
+{
+
+    /* 调用实例 
+     *  string xml = XmlUtil.Serializer(typeof(Student), stu1);
+     *  Student stu2 = XmlUtil.Deserialize(typeof(Student), xml) as Student;
+     *  xml = XmlUtil.Serializer(typeof(DataTable), dt1);
+     *  DataTable dt2 = XmlUtil.Deserialize(typeof(DataTable), xml) as DataTable;
+     *  xml = XmlUtil.Serializer(typeof(List<Student>), list1);
+     *  List<Student> list2 = XmlUtil.Deserialize(typeof(List<Student>), xml) as List<Student>;
+     */
+
+    /// <summary>
+    /// Xml序列化与反序列化
+    /// </summary>
+    public class XmlUtil
+    {
+        #region 反序列化
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="xml">XML字符串</param>
+        /// <returns></returns>
+        public static object Deserialize(Type type, string xml)
+        {
+            try
+            {
+                using (StringReader sr = new StringReader(xml))
+                {
+                    XmlSerializer xmldes = new XmlSerializer(type);
+                    return xmldes.Deserialize(sr);
+                }
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static object Deserialize(Type type, Stream stream)
+        {
+            XmlSerializer xmldes = new XmlSerializer(type);
+            return xmldes.Deserialize(stream);
+        }
+        #endregion
+
+        #region 序列化
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="obj">对象</param>
+        /// <returns></returns>
+        public static string Serializer(Type type, object obj)
+        {
+            MemoryStream Stream = new MemoryStream();
+            XmlSerializer xml = new XmlSerializer(type);
+            try
+            {
+                //序列化对象
+                xml.Serialize(Stream, obj);
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            Stream.Position = 0;
+            StreamReader sr = new StreamReader(Stream);
+            string str = sr.ReadToEnd();
+
+            sr.Dispose();
+            Stream.Dispose();
+
+            return str;
+        }
+
+        #endregion
+    }
+}
